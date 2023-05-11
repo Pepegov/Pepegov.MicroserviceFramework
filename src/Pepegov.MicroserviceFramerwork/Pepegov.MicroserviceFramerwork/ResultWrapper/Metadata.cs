800bf56b1fd58cef1c21ce535fefe8b70b54258a
pepegov <andelismore@gmail.com>
2023-05-11 12:25:18 +0300

    convert method in ResultWrapper

--- src/Pepegov.MicroserviceFramerwork/Pepegov.MicroserviceFramerwork/ResultWrapper/Metadata.cs
+++ src/Pepegov.MicroserviceFramerwork/Pepegov.MicroserviceFramerwork/ResultWrapper/Metadata.cs
@@ -2,15 +2,31 @@ namespace Pepegov.MicroserviceFramerwork.ResultWrapper;
 
 public class Metadata
 {
+    /// <summary>
+    /// Metadata description
+    /// </summary>
     public string Description { get; }
+
+    /// <summary>
+    /// Metadata type
+    /// </summary>
     public MetadataType Type { get; }
 
+    /// <summary>
+    /// Create metadata with type = info
+    /// </summary>
+    /// <param name="description"></param>
     public Metadata(string description)
     {
         Type = MetadataType.Info;
         Description = description;
     }
     
+    /// <summary>
+    /// Create metadata with your type 
+    /// </summary>
+    /// <param name="description"></param>
+    /// <param name="type"></param>
     public Metadata(string description, MetadataType type) : this(description)
     {
         Type = type;
--- src/Pepegov.MicroserviceFramerwork/Pepegov.MicroserviceFramerwork/ResultWrapper/ResultWrapper.cs
+++ src/Pepegov.MicroserviceFramerwork/Pepegov.MicroserviceFramerwork/ResultWrapper/ResultWrapper.cs
@@ -1,9 +1,19 @@
 namespace Pepegov.MicroserviceFramerwork.ResultWrapper;
 
+/// <summary>
+/// Helps to give api full response with exeptions and metadata 
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class ResultWrapper<T> where T : class 
 {
+    /// <summary>
+    /// Generalized message
+    /// </summary>
     public T? Message { get; set; }
 
+    /// <summary>
+    /// Check for exceptions
+    /// </summary>
     public bool IsSuccessful
     {
         get
@@ -17,9 +27,19 @@ public class ResultWrapper<T> where T : class
         }
     }
     
+    /// <summary>
+    /// Status code. Can be used for webapi or your own interpretation
+    /// </summary>
     public int StatusCode { get; set; }
 
+    /// <summary>
+    /// Exceptions received during result creation
+    /// </summary>
     public List<Exception>? Exceptions { get; private set; }
+    
+    /// <summary>
+    /// Additional response configuration data
+    /// </summary>
     public List<Metadata>? Metadatas { get; private set; }
 
     public ResultWrapper() {}
@@ -47,7 +67,11 @@ public class ResultWrapper<T> where T : class
         StatusCode = statusCode;
         AddException(exception);
     }
-
+    
+    /// <summary>
+    /// Add exception to field Exceptions
+    /// </summary>
+    /// <param name="exception"></param>
     public void AddException(Exception? exception)
     {
         if (exception is null)
@@ -64,6 +88,10 @@ public class ResultWrapper<T> where T : class
         Exceptions.Add(exception);
     }
 
+    /// <summary>
+    /// Add metadata to field Metadatas
+    /// </summary>
+    /// <param name="metadatas"></param>
     public void AddMetadatas(params Metadata[] metadatas)
     {
         if (Metadatas is null)
@@ -73,5 +101,16 @@ public class ResultWrapper<T> where T : class
         }
         
         Metadatas.AddRange(metadatas);
+    }
+
+    public ResultWrapper<TC> Convert<TC>(TC converObj) where TC : class
+    {
+        var result = new ResultWrapper<TC>();
+        result.Metadatas = this.Metadatas;
+        result.Exceptions = this.Exceptions;
+        result.StatusCode = this.StatusCode;
+        result.Message = converObj;
+
+        return result;
     }
 }

f5cce06212207c6606b07ea652a85a871b7f0f8f
pepegov <andelismore@gmail.com>
2023-05-23 16:38:14 +0300

    add MicroserviceMessageQueueException

---
+++ src/Pepegov.MicroserviceFramerwork/Pepegov.MicroserviceFramerwork/Exceptions/MicroserviceMessageQueueException.cs
@@ -0,0 +1,15 @@
+using System.Runtime.Serialization;
+
+namespace Pepegov.MicroserviceFramerwork.Exceptions;
+
+/// <summary>
+/// MessageQueue microservice exception
+/// </summary>
+[Serializable]
+public class MicroserviceMessageQueueException : Exception
+{
+    public MicroserviceMessageQueueException() { }
+    public MicroserviceMessageQueueException(string message) : base(message) { }
+    public MicroserviceMessageQueueException(string message, Exception innerException) : base(message, innerException) { }
+    protected MicroserviceMessageQueueException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}

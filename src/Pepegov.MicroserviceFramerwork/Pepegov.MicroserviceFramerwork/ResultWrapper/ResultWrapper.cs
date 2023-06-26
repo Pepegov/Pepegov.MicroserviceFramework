d50c042f66bba04865a5ba8d1643efeda282d0d8
pepegov <andelismore@gmail.com>
2023-06-26 12:42:42 +0300

    AddExceptions method

--- src/Pepegov.MicroserviceFramerwork/Pepegov.MicroserviceFramerwork/ResultWrapper/ResultWrapper.cs
+++ src/Pepegov.MicroserviceFramerwork/Pepegov.MicroserviceFramerwork/ResultWrapper/ResultWrapper.cs
@@ -92,6 +92,26 @@ public class ResultWrapper<T> where T : class
         
         Exceptions.AddRange(exceptions.ToExceptionData());
     }
+    
+    /// <summary>
+    /// Add exception datas to field Exceptions
+    /// </summary>
+    /// <param name="exceptions"></param>
+    public void AddExceptions(params ExceptionData[] exceptions)
+    {
+        if (exceptions is null)
+        {
+            return;
+        }
+        
+        if (Exceptions is null)
+        {
+            Exceptions = new List<ExceptionData>(exceptions) { };
+            return;
+        }
+        
+        Exceptions.AddRange(exceptions);
+    }
 
     /// <summary>
     /// Add metadata to field Metadatas

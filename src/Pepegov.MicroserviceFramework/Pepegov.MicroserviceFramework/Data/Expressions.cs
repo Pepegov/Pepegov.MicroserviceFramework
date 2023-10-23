06c52a0f132cf4484f69941406b63060d3e1d902
pepegov <andelismore@gmail.com>
2023-10-24 01:19:52 +0300

    add expressions

---
+++ src/Pepegov.MicroserviceFramework/Pepegov.MicroserviceFramework/Data/Expressions.cs
@@ -0,0 +1,33 @@
+using System.Text.RegularExpressions;
+
+namespace Pepegov.MicroserviceFramework.Data;
+
+public static class Expressions
+{
+    public static class Web
+    {
+        public static readonly Regex Url = new Regex(@"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)", RegexOptions.Singleline | RegexOptions.Compiled);
+        public static readonly Regex Email = new Regex(@"^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$", RegexOptions.Singleline | RegexOptions.Compiled);
+        public static readonly Regex IPv4 = new Regex(@"^\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b$", RegexOptions.Singleline | RegexOptions.Compiled);
+        public static readonly Regex IPv6 = new Regex(@"^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$", RegexOptions.Singleline | RegexOptions.Compiled);
+    }
+
+    public static class Paths
+    {
+        public static readonly Regex Windows = new Regex(
+            @"(^([a-z]|[A-Z]):(?=\\(?![\0-\37<>:"+"\""
+                 +@"/\\|?*])|\/(?![\0-\37<>:"+"\""+@"/\\|?*])|$)|^\\(?=[\\\/][^\0-\37<>:"+"\""
+                 +@"/\\|?*]+)|^(?=(\\|\/)$)|^\.(?=(\\|\/)$)|^\.\.(?=(\\|\/)$)|^(?=(\\|\/)[^\0-\37<>:"+"\""
+                 +@"/\\|?*]+)|^\.(?=(\\|\/)[^\0-\37<>:"+"\""
+                 +@"/\\|?*]+)|^\.\.(?=(\\|\/)[^\0-\37<>:"+"\""
+                 +@"/\\|?*]+))((\\|\/)[^\0-\37<>:"+"\""+
+                 @"/\\|?*]+|(\\|\/)$)*()$", RegexOptions.Singleline | RegexOptions.Compiled);
+        public static readonly Regex Linux = new Regex(@"^\/$|(^(?=\/)|^\.|^\.\.)(\/(?=[^/\0])[^/\0]+)*\/?$", RegexOptions.Singleline | RegexOptions.Compiled);
+    }
+
+    public static class PersonalData
+    {
+        public static readonly Regex CreditCardNumberExpression = new Regex(@"^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$", RegexOptions.Singleline | RegexOptions.Compiled);
+        public static readonly Regex PhoneNumberWithoutMaskExpression = new Regex(@"^(\+)(\d){10,14}$", RegexOptions.Singleline | RegexOptions.Compiled);   
+    }
+}
--- src/Pepegov.MicroserviceFramework/Pepegov.MicroserviceFramework/Infrastructure/Extensions/StringExtensions.cs
+++ src/Pepegov.MicroserviceFramework/Pepegov.MicroserviceFramework/Infrastructure/Extensions/StringExtensions.cs
@@ -1,16 +1,9 @@
-using System.Text.RegularExpressions;
+using Pepegov.MicroserviceFramework.Data;
 
 namespace Pepegov.MicroserviceFramework.Infrastructure.Extensions;
 
 public static class StringExtensions
 {
-    private static readonly Regex WebUrlExpression = new Regex(@"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)", RegexOptions.Singleline | RegexOptions.Compiled);
-    private static readonly Regex EmailExpression = new Regex(@"^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$", RegexOptions.Singleline | RegexOptions.Compiled);
-    private static readonly Regex IPv4Expression = new Regex(@"^\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b$", RegexOptions.Singleline | RegexOptions.Compiled);
-    private static readonly Regex IPv6Expression = new Regex(@"^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$", RegexOptions.Singleline | RegexOptions.Compiled);
-    private static readonly Regex CreditCardNumberExpression = new Regex(@"^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$", RegexOptions.Singleline | RegexOptions.Compiled);
-    private static readonly Regex PhoneNumberWithoutMaskExpression = new Regex(@"^(\+)(\d){10,14}$", RegexOptions.Singleline | RegexOptions.Compiled);
-    
     public static Guid ToGuid(this string str)
     {
         Guid.TryParse(str, out var result);
@@ -18,20 +11,20 @@ public static class StringExtensions
     }
     
     public static bool IsWebUrl(this string target)
-        => !string.IsNullOrEmpty(target) && WebUrlExpression.IsMatch(target);
+        => !string.IsNullOrEmpty(target) && Expressions.Web.Url.IsMatch(target);
 
     public static bool IsEmail(this string target)
-        => !string.IsNullOrEmpty(target) && EmailExpression.IsMatch(target);
+        => !string.IsNullOrEmpty(target) && Expressions.Web.Email.IsMatch(target);
     
     public static bool IsIPv4(this string target)
-        => !string.IsNullOrEmpty(target) && IPv4Expression.IsMatch(target);
+        => !string.IsNullOrEmpty(target) && Expressions.Web.IPv4.IsMatch(target);
     
     public static bool IsIPv6(this string target)
-        => !string.IsNullOrEmpty(target) && IPv6Expression.IsMatch(target);
+        => !string.IsNullOrEmpty(target) && Expressions.Web.IPv6.IsMatch(target);
     
     public static bool IsCreditCardNumber(this string target)
-        => !string.IsNullOrEmpty(target) && CreditCardNumberExpression.IsMatch(target);
+        => !string.IsNullOrEmpty(target) && Expressions.PersonalData.CreditCardNumberExpression.IsMatch(target);
 
     public static bool IsPhoneNumberWithoutMask(this string target)
-        => !string.IsNullOrEmpty(target) && PhoneNumberWithoutMaskExpression.IsMatch(target);
+        => !string.IsNullOrEmpty(target) && Expressions.PersonalData.PhoneNumberWithoutMaskExpression.IsMatch(target);
 }
